8b957f55927df281adc087dbf2d7991e218971a9
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-04-20 13:43:54 -0300

    Criando projeto com exemplo de using block

---
+++ UsingBlockSintax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace UsingBlockSintax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+                //quando chegar no final do bloco using todos os arquivos abertos serão fechados
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
