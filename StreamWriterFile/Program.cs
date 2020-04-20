a91c0f02ab9979934f778aabcc0aca43a335dd4e
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-04-20 13:56:31 -0300

    Criando projeto com exemplo de stream writer

---
+++ StreamWriterFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                //append text faz a escrita no final do arquivo
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
