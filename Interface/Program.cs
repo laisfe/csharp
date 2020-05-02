cc8ea55f4efa9b4610646c4f01427502d0a239c8
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-02 12:18:02 -0300

    Impelementando interface num projeto

---
+++ Interface/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Interface.Entities;
+using Interface.Services;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+
+            Console.Write("Car model: ");
+            string carModel = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+            rentalService.ProcessInvoic(carRental);
+
+            Console.WriteLine("INVOICE");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
