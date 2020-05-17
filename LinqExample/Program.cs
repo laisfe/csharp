d38b065c359476f457b0e8ec5ccaf8c423bbafa9
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-17 12:30:46 -0300

    Creating linq example project

---
+++ LinqExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the datasource
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
