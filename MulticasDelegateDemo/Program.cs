10c1de4e1f4c3bedebd3297e5e5bdccbb6e96524
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-17 12:27:41 -0300

    Creating multicast delegate project

---
+++ MulticasDelegateDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MulticasDelegateDemo.Services;
+
+namespace MulticasDelegateDemo
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //associa duas funções ao delegate 
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+        }
+    }
+}
