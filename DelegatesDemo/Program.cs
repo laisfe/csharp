247bf193c1e51b7d71266ac5b8816fae5cf34ecd
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-17 12:36:29 -0300

    Creating delegates demo project

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using DelegatesDemo.Services;
+using System;
+
+namespace DelegatesDemo
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
