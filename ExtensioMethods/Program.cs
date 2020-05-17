9a9f27088a3592a646874932b8da730754b69552
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-05-17 12:35:10 -0300

    Creating extension methods project

---
+++ ExtensioMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensioMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 05, 4, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
