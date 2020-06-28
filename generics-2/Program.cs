97ebcd0bb910711f677ee3768b5cef86c8542a8c
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-06-28 16:05:02 -0300

    Calculation project

---
+++ generics-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using generics_2.Entities;
+using generics_2.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace generics_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list); // <Product> is optional
+
+            Console.Write("Most expensive: ");
+            Console.WriteLine(p);
+        }
+    }
+}
