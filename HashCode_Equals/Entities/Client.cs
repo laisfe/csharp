90e210d02d320258a48330cf04335fc2580216d3
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-06-29 14:49:26 -0300

    Exemplo GetHashCode

---
+++ HashCode_Equals/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode_Equals.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+            Client other = obj as Client;
+
+            //o critério usado para definir se os objetos sao iguais ou não será o email
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ HashCode_Equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using HashCode_Equals.Entities;
+using System;
+
+namespace HashCode_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b); //retorna false pq a referência de memória dos dois objetos são diferentes
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
