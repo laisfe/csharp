2f230b11b0b9dc3d180391af202d18138c0f48f9
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-07-04 15:15:41 -0300

    Exercicio de conjunto utilizando HashSet

---
+++ Conjuntos/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conjuntos.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            //downcasting
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
